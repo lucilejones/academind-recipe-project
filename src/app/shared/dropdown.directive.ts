c514727bc6371f2793a16f284299db9c103a70fd
Lucile Jones <jones.lucile@gmail.com>
2023-10-10 13:13:49 -0600

    added dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen = false;
+
+    @HostListener('click') toggleOpen() {
+        this.isOpen = !this.isOpen;
+    }
+}
+
+
+// for a dropdown that can be closed by a click anywhere outside
+// replace the above code with what's below
+// (placing the listener on the document instead of the dropdown)
+
+// import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+// @Directive({
+//   selector: '[appDropdown]'
+// })
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen = false;
+//   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+//     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+//   }
+//   constructor(private elRef: ElementRef) {}
+// }
